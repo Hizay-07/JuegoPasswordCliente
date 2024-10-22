a93d25790bd99c2044dc7fe86588395da3e68e53
Oscar <hizzay@hotmail.com>
2024-10-22 12:48:01 -0600

    Ventana menu pincipal y personalizar perfil

---
+++ Vistas/VentanaMenuPrincipal.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Cliente.Vistas
+{
+    /// <summary>
+    /// Lógica de interacción para VentanaMenuPrincipal.xaml
+    /// </summary>
+    public partial class VentanaMenuPrincipal : Page
+    {
+        public VentanaMenuPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void PersonalizarPerfil(object sender, RoutedEventArgs e)
+        {
+            VentanaPersonalizarPerfil paginaPersonalizarPerfil = new VentanaPersonalizarPerfil();
+            this.NavigationService.Navigate(paginaPersonalizarPerfil);
+        }
+    }
+}
